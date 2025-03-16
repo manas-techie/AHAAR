3841cc7e4a7902c56e4c1037cc8f650697f8e03d
= <sayanmicrosoft857@gmail.com>
2025-03-16 22:59:21 +0530

    Navbar issue fixed

--- public/js/script.js
+++ public/js/script.js
@@ -19,21 +19,6 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
-// home page slider logic
-
-const slider = new A11YSlider(document.querySelector(".slider"), {
-  adaptiveHeight: false,
-  dots: true,
-  centerMode: true,
-  arrows: false,
-  responsive: {
-    480: {
-      dots: false, // dots enabled 1280px and up
-    },
-  },
-});
-
-
 
 // new form novalidate logic
 
